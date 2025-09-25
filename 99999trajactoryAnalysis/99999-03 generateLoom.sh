1724624daae6c83f2dcd3a243720938c207d2865
TuTandOvO <152852522+TuTandOvO@users.noreply.github.com>
2025-09-25 19:21:58 +0800

    Delete 99999trajactoryAnalysis directory

--- 99999trajactoryAnalysis/99999-03 generateLoom.sh
+++
@@ -1,8 +0,0 @@
-velocyto run10x \
-   /gpfsdata/home/renyixiang/SRTP/DeepVelo/vgMG_HFD \
-   /gpfsdata/home/renyixiang/cellranger/refdata-gex-mm10-2020-A/genes/genes.gtf
-
-velocyto run10x \
-   /gpfsdata/home/renyixiang/SRTP/DeepVelo/vgMG_ND \
-   /gpfsdata/home/renyixiang/cellranger/refdata-gex-mm10-2020-A/genes/genes.gtf
-
